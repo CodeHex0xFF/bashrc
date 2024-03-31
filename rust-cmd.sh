193ad5b19e27ed9b1a064da0394ae0e9ea6b87c6
hongdayu <hong_da_yu@163.com>
2024-03-31 21:58:41 +0800

    1.first init

---
+++ rust-cmd.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#替代 du
+cargo install du-dust
+cargo install fd-find
+cargo install bandwhich
+#替代grep
+cargo install ripgrep
+#替代ls
+cargo install lsd
+#代码统计工具
+cargo install --git https://github.com/XAMPPRocky/tokei.git tokei
+cargo install grex
+cargo install --locked watchexec-cli
+#替代df
+sudo apt install duf
+#替代curl
+curl -sfL https://raw.githubusercontent.com/ducaale/xh/master/install.sh | sh
+#替代rm
+cargo install rm-improved
+#替代cp
+cargo install xcp
+
+cargo install onefetch
